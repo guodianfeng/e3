09fa36e4505b6e0e21783922973781e52abe44a1
guodianfeng <gdf199099@163.com>
2017-09-26 17:17:07 +0800

    商品列表开发完毕

--- e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
+++ e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
@@ -1,9 +1,12 @@
 package cn.e3mall.service;
 
+import cn.e3mall.common.pojo.DataGridResult;
 import cn.e3mall.pojo.TbItem;
 
 public interface ItemService {
 	
 	TbItem getItemById(Long id);
+	//商品列表查询
+	DataGridResult getItemList(int page,int rows);
 
 }
--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -1,10 +1,17 @@
 package cn.e3mall.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import cn.e3mall.common.pojo.DataGridResult;
 import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.service.ItemService;
 
 @Service
@@ -17,6 +24,25 @@ public class ItemServiceImpl implements ItemService{
 	public TbItem getItemById(Long id) {
 		TbItem tbItem = tbItemMapper.selectByPrimaryKey(id);
 		return tbItem;
+	}
+
+	//商品列表分页查询
+	public DataGridResult getItemList(int page, int rows) {
+//		1、设置分页信息
+		PageHelper.startPage(page, rows);
+//		2、执行查询
+		TbItemExample example = new TbItemExample();
+		List<TbItem> list = tbItemMapper.selectByExample(example);
+//		3、从查询结果中取分页信息
+		PageInfo<TbItem> pageInfo=new PageInfo<>(list);
+		long total = pageInfo.getTotal();
+//		4、创建一个DataGridResult对象
+		DataGridResult dataGridResult = new DataGridResult();
+//		5、把结果封装到DataGridResult中
+		dataGridResult.setTotal(total);
+		dataGridResult.setRows(list);
+//		6、返回DataGridResult对象
+		return dataGridResult;
 	}
 
 }
